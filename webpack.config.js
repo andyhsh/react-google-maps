835d8dd7d28ecae592efa96b91e526818d91d038
Andy Ho <89andy@gmail.com>
2019-01-26 21:33:38 +0800

    Add babel loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   devServer: {
     contentBase: './dist'
   }
