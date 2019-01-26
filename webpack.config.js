77061e725f9c10a989c446c45436674f1d159e2f
Andy Ho <89andy@gmail.com>
2019-01-26 21:30:38 +0800

    Setup basic webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
