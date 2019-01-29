c5856771eac10adf486002531039caf3d3aa5025
Andy Ho <89andy@gmail.com>
2019-01-29 19:00:46 +0800

    Style button

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './Button.css';
 
 function Button(props) {
+  const disabled = props.disabled ? style.disabled : '';
+
   return (
-    <button onClick={props.onClick} disabled={props.disabled}>
-      {props.content}
+    <button
+      className={`${style.button} ${disabled}`}
+      onClick={props.onClick}
+      disabled={props.disabled || props.loading}
+    >
+      {!props.loading && props.content}
+      {props.loading && <div className={style.loading} />}
     </button>
   );
 }
@@ -13,6 +21,7 @@ Button.propTypes = {
   content: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
+  loading: PropTypes.bool,
 };
 
 export default Button;
